2499f35c71528cedb09d475ea83d66252e3fae90
Antonio Sulejman <tocycz@gmail.com>
2022-04-16 00:06:01 +0200

    feat:add first test for app

--- src/Main.elm
+++ src/Main.elm
@@ -5,4 +5,16 @@ import Html
 
 main : Html.Html msg
 main =
-    Html.text "Hello fuzz"
+    Html.text (shrinkText 20 "Hello fuzz this is a longer string")
+
+
+shrinkText max text =
+    if String.length text <= max then
+        text
+
+    else
+        let
+            extra =
+                String.length text - max
+        in
+        String.dropRight extra text ++ " ..."
---
+++ tests/MyFirstTest.elm
@@ -0,0 +1,19 @@
+module MyFirstTest exposing (..)
+
+import Expect
+import Test exposing (..)
+
+
+test1 =
+    test "1+1 should be 2" test1Plus1
+
+
+test1Plus1 _ =
+    let
+        result =
+            1 + 1
+
+        ok =
+            result == 2
+    in
+    Expect.true "1 + 1" ok
